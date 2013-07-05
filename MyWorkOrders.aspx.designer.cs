137ee02c5db0afc4ce6f9fd4ab26ae3b5c001e9b
NorfolkAdam <adam.wills@norfolkcounty.ca>
2013-07-05 10:31:01 -0400

    Added Create New WO Button
    closes #8

--- MyWorkOrders.aspx.designer.cs
+++ MyWorkOrders.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace HNHUWO2 {
     public partial class MyWorkOrders {
         
         /// <summary>
+        /// navCreateNew control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::HNHUWO2.NavButton navCreateNew;
+        
+        /// <summary>
         /// ddItemsPerPage control.
         /// </summary>
         /// <remarks>
