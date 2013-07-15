6f4a9427483db7b6cc5bec5351896e0716ac32ff
NorfolkAdam <adam.wills@norfolkcounty.ca>
2013-07-15 15:25:20 -0400

    Added Admin section to menu

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -112,6 +112,24 @@ namespace HNHUWO2 {
         protected global::System.Web.UI.WebControls.HyperLink lnkDesigners1;
         
         /// <summary>
+        /// adminMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl adminMenu;
+        
+        /// <summary>
+        /// lnkAdmin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkAdmin;
+        
+        /// <summary>
         /// reportsMenu control.
         /// </summary>
         /// <remarks>
