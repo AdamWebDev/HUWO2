dc75168806d47e2f902ec8eaceb296c230969994
NorfolkAdam <adam.wills@norfolkcounty.ca>
2013-07-17 12:08:42 -0400

    Added Session Handler

---
+++ Classes/MySession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HNHUWO2.Classes
+{
+    public class MySession
+    {
+        public static MySession Current
+        {
+            get
+            {
+                MySession session = (MySession)HttpContext.Current.Session["__MySession__"];
+                if (session == null)
+                {
+                    session = new MySession();
+                    HttpContext.Current.Session["__MySession__"] = session;
+                }
+                return session;
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string UserName { get; set; }
+        public int? Role { get; set; }
+    }
+}
