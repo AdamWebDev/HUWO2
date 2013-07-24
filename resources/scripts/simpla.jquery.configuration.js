f96e1766d4250eb5a13038ab2c0b68d74025c9b7
NorfolkAdam <adam.wills@norfolkcounty.ca>
2013-07-24 16:27:56 -0400

    Add magnefic modal

--- resources/scripts/simpla.jquery.configuration.js
+++ resources/scripts/simpla.jquery.configuration.js
@@ -33,19 +33,10 @@ $(document).ready(function () {
 		);
 
 
-
-    //Close button:
-
-    $(".close").click(
-			function () {
-			    $(this).parent().fadeTo(400, 0, function () { // Links with the class "close" will close parent
-			        $(this).slideUp(400);
-			    });
-			    return false;
-			}
-		);
-
-  
+			$('.open-popup-link').magnificPopup({
+            type: 'inline'
+        
+    });
 
     $('.datepicker').datepicker({
         dateFormat: "yy-mm-dd"
